8fcc53b510baebdfb66ca99fab03c734426fbe14
Konark-2ez <konarkkumar456@gmail.com>
2023-07-03 13:05:11 +0530

    add

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const jsonserver = require("json-server");
+const cors = require("cors");
 const server = jsonserver.create();
+
 const router = jsonserver.router("db.json");
 const middlewares = jsonserver.defaults();
 const port = 3000;
-
+server.use(cors())
+server.use(jsonserver.bodyParser)
 server.use(middlewares)
 server.use(router)
 server.listen(port,()=>{
